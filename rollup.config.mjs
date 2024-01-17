e24ddd45ffaa38c366f99e61d409644b53c38f8e
Scott Bedard <scottbedard90@gmail.com>
2024-01-16 21:38:10 -0600

    0.0.1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'rollup'
 import del from 'rollup-plugin-delete'
-import pkg from './package.json' assert { type: "json" }
+import pkg from './package.json' assert { type: 'json' }
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,18 +1,17 @@
-const exec = require('child_process').execSync
-const fs = require('fs')
-const path = require('path')
-const pkg = require('../package.json')
+import { fileURLToPath } from 'url'
+import exec from 'child_process'
+import fs from 'fs'
+import path from 'path'
+import pkg from '../package.json' assert { type: 'json' }
 
-const indexPath = path.resolve(__dirname, '../src/index.ts')
+const indexPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../src/index.ts')
 
-async function cli() {
+function cli() {
   const src = String(fs.readFileSync(indexPath)).replace('x.y.z', pkg.version)
 
   fs.writeFileSync(indexPath, src)
 
-  exec('npm run build && npm publish')
+  exec.execSync('npm run build && npm publish')
 }
 
-if (require.main === module) {
-  cli()
-}
+cli()
