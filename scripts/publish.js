51d19f2782fe0168143a9df303d798a9e1d2cffc
Scott Bedard <scottbedard90@gmail.com>
2024-01-16 21:44:36 -0600

    publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,17 +1,18 @@
-import { fileURLToPath } from 'url'
-import exec from 'child_process'
-import fs from 'fs'
-import path from 'path'
-import pkg from '../package.json' assert { type: 'json' }
+const exec = require('child_process').execSync
+const fs = require('fs')
+const path = require('path')
+const pkg = require('../package.json')
 
-const indexPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../src/index.ts')
+const indexPath = path.resolve(__dirname, '../src/index.ts')
 
-function cli() {
+async function cli() {
   const src = String(fs.readFileSync(indexPath)).replace('x.y.z', pkg.version)
 
   fs.writeFileSync(indexPath, src)
 
-  exec.execSync('npm run build && npm publish')
+  exec('npm run build && npm publish')
 }
 
-cli()
+if (require.main === module) {
+  cli()
+}
