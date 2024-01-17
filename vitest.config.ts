9c1773e5be8422937508b9100939c2683a777fd2
Scott Bedard <scottbedard90@gmail.com>
2024-01-16 21:39:39 -0600

    exclude ci scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [
+        'scripts/**/*',
+      ],
+    },
     environment: 'happy-dom',
   },
 })
